870c999ea8ca960687b75c4526ea0976e838b84b
David Shustin <david.shustin@gmail.com>
2016-11-12 17:55:02 -0500

    Completed basics!

--- Assets/ControllerMovement.cs
+++ Assets/ControllerMovement.cs
@@ -13,44 +13,35 @@ public class ControllerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< movement
-        float speed = 1;
-        Vector3 direction = new Vector3(0, 0, 0);
-=======
         
         float x = 0, z = 0;
-        Debug.Log("1");
         if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.Gamepad)) {
             Debug.Log("Pressing 1");
         }
->>>>>>> local
         if (holding(OVRInput.Button.Up))
         {
-            Debug.Log("UP");
+			z++;
         }
         if (holding(OVRInput.Button.Down))
         {
-            Debug.Log("DOWN");
+			z--;
         }
         if (holding(OVRInput.Button.Left))
         {
-            Debug.Log("LEFT");
+			x--;
         }
         if (holding(OVRInput.Button.Right))
         {
-            Debug.Log("RIGHT");
-        }
-<<<<<<< movement
-	}
+			x++;
+    	}
 
-=======
+
 		Vector3 f = tr.forward * z + tr.right * x;
 		f.y = 0;
 		trf.position += f * Time.deltaTime * speed;
 	}
 
 
->>>>>>> local
     private bool holding(OVRInput.Button butt) {
         return OVRInput.Get(butt, OVRInput.Controller.Remote) || OVRInput.Get(butt, OVRInput.Controller.Gamepad);
     }
